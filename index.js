c0bad142a3b75678ad34ff10225e80b59f4da355
Aj <ajayy101@gmail.com>
2022-12-08 10:21:28 -0500

    updated log action payload

--- index.js
+++ index.js
@@ -8,7 +8,14 @@ try {
   const time = new Date().toTimeString();
   core.setOutput("time", time);
   // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context, undefined, 2);
+  const payload = JSON.stringify(
+    {
+      event_name: github.event_name,
+      event: github.event,
+    },
+    undefined,
+    2
+  );
   console.log(`The event payload: ${payload}`);
 } catch (error) {
   core.setFailed(error.message);
